bdb259b8d867b3930823ce1c982ccfbfd3e65384
Laia <laia.fustercorral@gmail.com>
2024-03-09 19:14:21 +0100

    Project creation

---
+++ Forever/Source/Forever/Forever.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Forever.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Forever, "Forever" );
